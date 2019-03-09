e779f03a8da15ea4d29443c63a913c80432516a5
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-09 17:39:34 +0200

    Added form validation.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Post } from './../post.model';
 
 @Component({
@@ -15,14 +16,19 @@ export class PostCreateComponent {
 
   constructor() {}
 
-  onSavePost() {
+  onSavePost(postForm: NgForm) {
+    if (postForm.invalid) {
+      return;
+    }
+
     const post: Post = {
-      title: this.postTitle,
-      content: this.postContent
+      title: postForm.value.postTitle,
+      content: postForm.value.postContent
     };
+
     console.log(post);
     this.postCreated.emit(post);
-    this.postTitle = '';
-    this.postContent = '';
+    postForm.value.postTitle = '';
+    postForm.value.postContent = '';
   }
 }
