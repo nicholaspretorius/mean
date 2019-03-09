c53ea63caaed59748e64649c3a8cffaffc551038
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-09 12:05:43 +0200

    Added some base styles.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: []
+  styleUrls: ['./post-create.component.scss']
 })
 export class PostCreateComponent {
   constructor() {}
